d2216fdca5503de064da4606fc80375f962b1ee6
DartRuffian <skulldude327@gmail.com>
2023-12-03 02:19:50 -0600

    Fixed build script

--- Tools/build.bat
+++ Tools/build.bat
@@ -1,5 +1,5 @@
 set source="C:\Coding\GitHub Repos\BC-Origins\source"
-set target="%appdata%\com.modrinth.theseus\profiles\BC Cobblemon [TEST]\saves\Origin Testing\datapacks"
+set target="%appdata%\com.modrinth.theseus\profiles\BC Cobblemon [TEST]\saves\Origin Testing\datapacks\BC-Origins"
 
 rmDir /s /q %target%
 
